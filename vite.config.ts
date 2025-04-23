dc1246716dd117fac15733bdbabdb998c92b437d
Arjan van Wijk <thanarie@gmail.com>
2025-04-23 15:17:43 +0200

    Use cloudflare vite plugin

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,11 @@ import path from 'path';
 import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
+import { cloudflare } from '@cloudflare/vite-plugin';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), cloudflare()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
